0fddcb5ddbe9aa7709d2be92bd7d335c9377e6c1
Nicholas Werner <nicwerner2515@gmail.com>
2018-11-14 23:01:03 -0500

    added list view still needs some work

--- StudioProjects/Find_My_Freaking_Prop/app/src/main/java/com/example/administrator/find_my_freaking_prop/MainActivity.java
+++ StudioProjects/Find_My_Freaking_Prop/app/src/main/java/com/example/administrator/find_my_freaking_prop/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.administrator.find_my_freaking_prop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
+
+    String[] items = {"Props"};
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values);
+
 
     //create MyDatabaseHelper Object
     MyDatabaseHelper myDb;
@@ -13,4 +18,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         myDb = new MyDatabaseHelper(this);
     }
+
+
 }
